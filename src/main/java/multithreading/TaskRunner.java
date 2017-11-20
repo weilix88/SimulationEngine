cb70bcdd807e9402723a49a56daf223809341b7c
weilix88 <weilix88@gmail.com>
2017-11-20 00:09:12 -0500

    produce eso file

--- src/main/java/multithreading/TaskRunner.java
+++ src/main/java/multithreading/TaskRunner.java
@@ -254,6 +254,7 @@ public class TaskRunner implements Runnable {
                 access.set("Taskhtml#"+requestId, readTextFile(path+"IDFTable.html"));
                 access.set("Taskerr#"+requestId, readTextFile(path+"IDF.err"));
                 access.set("Taskcsv#"+requestId, readTextFile(path+"IDF.csv"));
+                access.set("Taskeso#"+requestId, readTextFile(path+"IDF.eso"));
 
                 access.rpush("TaskStatus#"+requestId, "Status_FINISHED");
                 
